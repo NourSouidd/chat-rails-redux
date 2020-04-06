d195140ffded7b24230d926c5a7f745a4c400a8d
Nour Souid <noursouidd@gmail.com>
2020-04-06 11:51:46 -0400

    destroy all the models in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seeding"
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+puts "Seeding done!"
