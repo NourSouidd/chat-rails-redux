c7e0d7b9b8fa425118f8fe197dc444c6eb6f5371
Nour Souid <noursouidd@gmail.com>
2020-04-06 13:52:55 -0400

    index action rendres the list of messages as json

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::MessagesController < ApplicationController
+  # before_action :find_channel_name
   def index
+   messages = Message.order(created_at: :desc)
+   render json: messages
   end
 
   def create
   end
+
+  # private
+
+  # def find_channel_name(channel_name)
+  #   Channel.find_by(name: channel_name)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   end
 
   resources :channels, only: [ :show ]
-  root to: 'channels#show'
+  # root to: 'channels#show'
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
