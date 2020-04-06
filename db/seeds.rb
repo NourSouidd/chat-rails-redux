8391d39672f913ae97f744a085ef226512acc6c2
Nour Souid <noursouidd@gmail.com>
2020-04-06 13:27:08 -0400

    added users and messages to the seed file with the faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 puts "seeding"
 
 Message.destroy_all
 User.destroy_all
 Channel.destroy_all
+
+general = Channel.create!(name: "general")
+paris = Channel.create!(name: "paris")
+react = Channel.create!(name: "react")
+
+10.times do
+  channels = [general, paris, react]
+  random = channels.sample
+  ghost_character = Faker::Movies::Ghostbusters.character
+  email = Faker::Internet.email(name: ghost_character, domain: "ghostbuster")
+  user = User.create!(email: email, password: "123456")
+  5.times do
+    content = Faker::Movies::Ghostbusters.quote
+    message = Message.create!(user: user , channel: random , content: content)
+  end
+
+end
+
+
+java = User.create!(email: "java@react.com" , password: "123456")
+
+message = Message.create!(user: java , channel: general , content: "I made it!")
 
 puts "Seeding done!"
