d51ced8a2c836be087e15f47a175544609dd7df7
Nour Souid <noursouidd@gmail.com>
2020-04-07 01:29:46 -0400

    implimentent the email to the json in the message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,18 @@ class Message < ApplicationRecord
   belongs_to :channel
   belongs_to :user
   validates :content, presence: true
+
+  def as_json(options = {})
+
+    email = user.email
+
+    {
+      id: id,
+      email: email,
+      content: content,
+      created_at: created_at,
+      channel: channel.name
+    }
+  end
+
 end
